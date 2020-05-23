34a282b1a2d1d0906cf364220ca0cd1d7a6aa561
wangwy <448261725@qq.com>
2020-05-23 23:35:58 +0800

    第一次提交class03

---
+++ src/com/wwy/utils/TestClass03.java
@@ -0,0 +1,12 @@
+package com.wwy.utils;
+
+/**
+ * @Auther: wwy
+ * @Date: 2020/5/23 23:34
+ * @Description: com.wwy.utils
+ * @version: 1.0
+ */
+public class TestClass03 {
+    private String  name;
+
+}
