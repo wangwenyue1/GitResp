ad31cdff25dd4ad10c4301481f9d36196aa233fb
wangwy <448261725@qq.com>
2020-05-23 23:49:22 +0800

    第2次提交class03

---
+++ src/com/wwy/utils/Persion.java
@@ -0,0 +1,12 @@
+package com.wwy.utils;
+
+/**
+ * @Auther: wwy
+ * @Date: 2020/5/23 23:48
+ * @Description: com.wwy.utils
+ * @version: 1.0
+ */
+public class Persion {
+    private String name;
+
+}
